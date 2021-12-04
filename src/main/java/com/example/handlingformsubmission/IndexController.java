ee458dcffae99037d53ae1649e0e88a13f27d12c
Shashwat Anand <shashwatanand1801@gmail.com>
2021-12-05 01:49:57 +0530

    new main branch

--- src/main/java/com/example/handlingformsubmission/IndexController.java
+++ src/main/java/com/example/handlingformsubmission/IndexController.java
@@ -1,27 +1,20 @@
 package com.example.handlingformsubmission;
- 
+
 import java.util.concurrent.ExecutionException;
- 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
- 
+
 @Controller
-public class AdminController {
+public class IndexController {
 	
-	@GetMapping("/admin_login")
+	@GetMapping("/index")
 	public String login(Model model) {
-		return "admin_login";
+		return "index";
 	}
- @GetMapping("/admin_dashboard")
-	public String dashboard(Model model) {
-		return "admin_dashboard";
-	}
- // @GetMapping("/viewWorker")
-	// public String worker(Model model) {
-	// 	return "viewWorker";
-	// }
+ 
 }
---
+++ src/main/java/com/example/handlingformsubmission/UserController.java
@@ -0,0 +1,38 @@
+package com.example.handlingformsubmission;
+ 
+import java.util.concurrent.ExecutionException;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+ 
+@Controller
+public class UserController {
+	
+ @GetMapping("/viewUser")
+	public String viewUser(Model model) {
+		return "viewUser";
+	}
+ @GetMapping("/user_login")
+	public String userLogin(Model model) {
+		return "user_login";
+	}
+ 
+ @GetMapping("/user_signup")
+	public String userSignup(Model model) {
+		return "user_signup";
+	}
+
+	@GetMapping("/user_dashboard")
+	public String userDashboard(Model model){
+		return "user_dashboard";
+	}
+
+	@GetMapping("/booking")
+	public String booking(Model model){
+		return "booking";
+	}
+}
